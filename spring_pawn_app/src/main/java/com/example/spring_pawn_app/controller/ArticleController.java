8a60eb956c2c092fad3c3500a6badf013744128c
congtan1610 <congtan125@gmail.com>
2023-05-12 20:46:24 +0700

    up

--- spring_pawn_app/src/main/java/com/example/spring_pawn_app/controller/ArticleController.java
+++ spring_pawn_app/src/main/java/com/example/spring_pawn_app/controller/ArticleController.java
@@ -3,36 +3,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-
 public class ArticleController {
-man
-  public void save(){
-        String name = "hello";
-    }
 
-    public void save(){
-        String name = "hello";
-    }
-
-    public void save(){
-        String name = "hello";
-    }
-
-    public void save(){
-        String name = "hello";
-    }
-
-  trung
-    24343242
-    34
-    234
-    324
-    324
-    324
-    32
-    423
-    432
-    432
-    423
-      
 }
