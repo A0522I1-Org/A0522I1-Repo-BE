12191fc6ec3e60abea63c34d93746fb273aae7b5
ADMIN-PC\ASUS <vodinhtuan174@gmail.com>
2023-05-12 19:43:59 +0700

    update

---
+++ spring_pawn_app/src/main/java/com/example/spring_pawn_app/service/user_has_role/UserHasRoleService.java
@@ -0,0 +1,7 @@
+package com.example.spring_pawn_app.service.user_has_role;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserHasRoleService implements IUserHasRoleService{
+}
