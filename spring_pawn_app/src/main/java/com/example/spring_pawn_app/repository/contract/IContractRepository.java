1cb4c4d2b35c8c9e56dbfb43e50304b1e9188761
A0322I1huynhvannam <huynhvannam.tk1@gmail.com>
2023-05-15 18:54:17 +0700

    update

--- spring_pawn_app/src/main/java/com/example/spring_pawn_app/repository/contract/IContractRepository.java
+++ spring_pawn_app/src/main/java/com/example/spring_pawn_app/repository/contract/IContractRepository.java
@@ -2,7 +2,13 @@ package com.example.spring_pawn_app.repository.contract;
 
 import com.example.spring_pawn_app.model.Contract;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 @Repository
 public interface IContractRepository extends JpaRepository<Contract, Integer>{
+    @Query(value = "select c.id,c.contract_code, c.customer_id, c.begin_date, c.end_date, c.interest, c.product_id, c.employee_id, c.status_id, c.is_flag from contract as c where c.id = :id",
+            countQuery ="select c.id,c.contract_code, c.customer_id, c.begin_date, c.end_date, c.interest, c.product_id from contract as c where c.id = :id" ,
+            nativeQuery = true)
+    Contract findContractById(@Param("id") Integer id);
 }
--- spring_pawn_app/src/main/java/com/example/spring_pawn_app/service/contract/IContractService.java
+++ spring_pawn_app/src/main/java/com/example/spring_pawn_app/service/contract/IContractService.java
@@ -1,4 +1,5 @@
 package com.example.spring_pawn_app.service.contract;
 
 public interface IContractService {
+    
 }
