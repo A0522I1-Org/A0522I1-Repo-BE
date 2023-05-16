d4bf6f1211b9ed4cd6b50378cec6c25d7bd4e7f5
vohoang13 <vanhoang132003@gmail.com>
2023-05-17 01:19:27 +0700

    HoangVV update

--- spring_pawn_app/src/main/java/com/example/spring_pawn_app/dto/contract/IContractRepository.java
+++ spring_pawn_app/src/main/java/com/example/spring_pawn_app/dto/contract/IContractRepository.java
@@ -1,4 +1,4 @@
-package com.example.spring_pawn_app.repository.contract;
+package com.example.spring_pawn_app.dto.contract;
 
 import com.example.spring_pawn_app.dto.ContractDto;
 import com.example.spring_pawn_app.model.Contract;
--- spring_pawn_app/src/main/java/com/example/spring_pawn_app/service/contract/ContractService.java
+++ spring_pawn_app/src/main/java/com/example/spring_pawn_app/service/contract/ContractService.java
@@ -2,12 +2,11 @@ package com.example.spring_pawn_app.service.contract;
 
 import com.example.spring_pawn_app.dto.ContractDto;
 import com.example.spring_pawn_app.model.Contract;
-import com.example.spring_pawn_app.repository.contract.IContractRepository;
+import com.example.spring_pawn_app.dto.contract.IContractRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
