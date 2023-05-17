6eddd3fc9b7d73cc2c97dbee6a9db56bb4883cd1
ADMIN-PC\ASUS <vodinhtuan174@gmail.com>
2023-05-17 18:10:51 +0700

    up

--- spring_pawn_app/src/main/java/com/example/spring_pawn_app/service/customer/ICustomerService.java
+++ spring_pawn_app/src/main/java/com/example/spring_pawn_app/service/customer/ICustomerService.java
@@ -1,4 +1,9 @@
 package com.example.spring_pawn_app.service.customer;
 
+import com.example.spring_pawn_app.dto.customer.CustomerDTODetail;
+
+import java.util.Optional;
+
 public interface ICustomerService {
+    Optional<CustomerDTODetail> getCustomerById(Integer id);
 }
