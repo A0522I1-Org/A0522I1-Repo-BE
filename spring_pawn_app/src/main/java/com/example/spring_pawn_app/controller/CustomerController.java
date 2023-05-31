22ad3b5592f3e2f10ca5426f40ca10a462b65f7a
pacry241 <phanduyman241@gmail.com>
2023-05-31 15:29:09 +0700

    up 31May

--- spring_pawn_app/src/main/java/com/example/spring_pawn_app/controller/CustomerController.java
+++ spring_pawn_app/src/main/java/com/example/spring_pawn_app/controller/CustomerController.java
@@ -2,22 +2,30 @@ package com.example.spring_pawn_app.controller;
 
 import com.example.spring_pawn_app.dto.customer.CustomerRegisterDTO;
 import com.example.spring_pawn_app.model.Customer;
-import com.example.spring_pawn_app.service.customer.CustomerService;
+import com.example.spring_pawn_app.service.customer.ICustomerService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
+@RequestMapping("api")
 @CrossOrigin("*")
 public class CustomerController {
     @Autowired
-    private CustomerService customerService;
-    @PostMapping("customer")
-    public ResponseEntity<Customer> addNewCustomer(@RequestBody CustomerRegisterDTO customerRegisterDTO) {
+    private ICustomerService iCustomerService;
+
+    // add new customer by ManPD
+    //        Start
+    @PostMapping("dangkynhanh")
+    public ResponseEntity<Customer> addNewCustomer(@Valid @RequestBody CustomerRegisterDTO customerRegisterDTO) {
         Customer customer = new Customer();
-        BeanUtils.copyProperties(customerRegisterDTO,customer);
-        return new ResponseEntity<>(customerService.createCustomer(customer), HttpStatus.CREATED);
+        BeanUtils.copyProperties(customerRegisterDTO, customer);
+        return new ResponseEntity<>(iCustomerService.createCustomer(customer), HttpStatus.CREATED);
     }
+    //    End
 }
--- spring_pawn_app/src/main/java/com/example/spring_pawn_app/service/customer/ICustomerService.java
+++ spring_pawn_app/src/main/java/com/example/spring_pawn_app/service/customer/ICustomerService.java
@@ -3,6 +3,5 @@ package com.example.spring_pawn_app.service.customer;
 import com.example.spring_pawn_app.model.Customer;
 
 public interface ICustomerService {
-
     Customer createCustomer(Customer customer);
 }
