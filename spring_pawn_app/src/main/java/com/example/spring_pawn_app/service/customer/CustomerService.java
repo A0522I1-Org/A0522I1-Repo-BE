e4ee9290d13c87a8cf95c189d4ea3a799af894cc
Vodinhtuan <vodinhtuan174@gmail.com>
2023-06-02 21:33:31 +0700

    Committing local changes before branch switch

---
+++ spring_pawn_app/src/main/java/com/example/spring_pawn_app/service/customer/CustomerService.java
@@ -0,0 +1,8 @@
+package com.example.spring_pawn_app.service.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService implements ICustomerService{
+
+}
---
+++ spring_pawn_app/src/main/java/com/example/spring_pawn_app/service/user/UserService.java
@@ -0,0 +1,7 @@
+package com.example.spring_pawn_app.service.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements IUserService{
+}
